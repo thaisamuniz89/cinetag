707425da921f3cffff6eabfd1e1e0ccedac34fce
Thaisa Muniz <thaisamuniz89@gmail.com>
2023-02-24 16:02:57 -0300

    update
    Curso REACTJS

---
+++ src/pages/PaginaBase/index.js
@@ -0,0 +1,20 @@
+import Cabecalho from "components/Cabecalho";
+import Container from "components/Container";
+import Rodape from "components/Rodape";
+import FavoritosProvider from "contexto/Favoritos";
+import { Outlet } from "react-router-dom";
+
+function PaginaBase() {
+    return (
+        <main>
+            <Cabecalho />
+            <FavoritosProvider>
+                <Container>
+                    <Outlet />
+                </Container>
+            </FavoritosProvider>
+            <Rodape />
+        </main>
+    )
+}
+export default PaginaBase;
